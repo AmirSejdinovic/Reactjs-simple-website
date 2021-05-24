fcbcd3960653380ff41f59f27ee9eb7f3a4f094f
Amir Sejdinovic <amir24V@yahoo.com>
2021-05-24 10:20:57 +0200

    Creating  routes and blank components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+//Importing boostrap from node modules
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//Importing the components 
+import Home from './Routes/Home';
+import About from './Routes/About';
+import Contact from './Routes/Contact';
+//Importing react router dom package
+import {BrowserRouter,Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+         <Route exact path="/" component={Home}/>
+         <Route exact path="/about" component={About} />
+         <Route exact path="/contact" component={Contact}/>
+      </BrowserRouter>
     </div>
   );
 }
